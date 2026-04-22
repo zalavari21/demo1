68b3458dbe15da76a57d41e95f613e587a7e11ac
zalavari21 <101672282+zalavari21@users.noreply.github.com>
2026-04-22 22:30:53 +0200

    Refactor ujUgyfel and listaFrissit functions

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
-initDB().then(() => listaFrissit());
+initDB().then(() => listaFrissit()); 
 
-function ujUgyfel() {
-  let nev = document.getElementById("nev").value;
-  let cim = document.getElementById("cim").value;
+function ujUgyfel() { 
 
-  let now = Date.now();
+//let szoveg = document.getElementById("szoveg").value;
+let szoveg = "szoveg"; 
 
-  db.run(
-    "INSERT INTO ugyfelek (nev, cim, modositva, sync) VALUES (?, ?, ?, 0)",
-    [nev, cim, now]
-  );
+ let d = new Date(Date.now());
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, '0'); 
+  const day = String(d.getDate()).padStart(2, '0');
+  
+  const hours = String(d.getHours()).padStart(2, '0');
+  const minutes = String(d.getMinutes()).padStart(2, '0');
+  const seconds = String(d.getSeconds()).padStart(2, '0');
 
-  saveDB();
-  listaFrissit();
+const datum = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+
+
+db.run( 
+"INSERT INTO logok (szoveg, modositva) VALUES (?, ?)", 
+[szoveg, datum] ); 
+
+saveDB(); 
+listaFrissit(); 
+document.getElementById("szoveg").value = '';
+document.getElementById("szoveg").focus();
+
+} 
+
+function loguritk() {
+db.run("DELETE FROM éogok");
+saveDB();
+
 }
 
-function listaFrissit() {
-  let res = db.exec("SELECT * FROM ugyfelek ORDER BY id DESC");
+function listaFrissit() { 
 
-  let html = "";
-  if (res.length > 0) {
-    for (let row of res[0].values) {
-      html += `<div>
-        <b>${row[1]}</b> – ${row[2]}
-      </div>`;
-    }
-  }
-  document.getElementById("lista").innerHTML = html;
-}
+let res = db.exec("SELECT * FROM logok ORDER BY id DESC limit 5"); 
+
+let html = ""; 
+if (res.length > 0) { 
+	for (let row of res[0].values) { 
+	  html += `<div> 
+	    <b>${row[1]}</b> – ${row[2]} </div>`; 
+		
+		} 
+		
+		} 
+		
+		document.getElementById("lista").innerHTML = html; 
+	    document.getElementById("szoveg").focus();
+		
+		} 
